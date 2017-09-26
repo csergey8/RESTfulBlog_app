7a70c08d525e6f441786399fdd6fc8b0cbb3b29e
csergey8 <c.sergey@protonmail.com>
2017-09-26 18:04:15 +0300

    add mongoose config

--- app.js
+++ app.js
@@ -2,3 +2,42 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+
+
+//Mongodb connect
+mongoose.connect('mongodb://127.0.0.1/data', function (err) {
+    console.log('base connected');
+});
+
+
+//App config
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    exteded: true
+}));
+
+/// Mongoose model config
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+
+
+
+
+
+
+
+//Server listen
+app.listen(3000, '127.0.0.1', function (err) {
+    console.log('server start');
+})
